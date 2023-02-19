c4d4c1d01e15221f698c8698ae31c613b6f68aa9
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-02-19 14:00:08 -0300

    update

---
+++ Manipulando_Dados/6-Maiusculas_Minusculas.js
@@ -0,0 +1,6 @@
+//Manipulando Strings e Numbers
+//Transforme letras minúsculas em maiúsculas. Faça o contrário disso também
+
+let phrase = 'Programar é muito bacana!'
+console.log(phrase.toUpperCase())
+console.log(phrase.toLowerCase())
