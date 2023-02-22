6a782fb5936b96a9b750b39b4c6fc9103deeee6c
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-02-22 13:00:58 -0300

    update

---
+++ Expressoes_e_Operadores/1-typeOfDelete.js
@@ -0,0 +1,11 @@
+// Operadores Unários
+// TypeOf
+// Delete
+
+const person = {
+    name: "Clecio",
+    age: 25,
+}
+delete person.age //Deleta a propriedade de dentro do objeto
+
+console.log(person)
