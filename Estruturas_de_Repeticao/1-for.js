d726e81ebd5c17d8731e759ddf7b6506584cfaf5
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-02-24 19:34:58 -0300

    update

---
+++ Estruturas_de_Repeticao/1-for.js
@@ -0,0 +1,12 @@
+//Estrutura de repetição
+//for (para)
+//break - para a execução do loop
+//continue - pula o número selecionado e continua a contagem
+
+for (let i = 10; i > 0; i--) {
+    if (i === 5) {
+        continue;
+    }
+
+    console.log(i)
+}
