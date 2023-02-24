3ee6ac67ca4f878f5ef284bbefaa800c0b184bad
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-02-24 19:44:11 -0300

    update

---
+++ Estruturas_de_Repeticao/2-while.js
@@ -0,0 +1,10 @@
+//Estrutura de repetição
+//while (enquanto)
+//usamos muito o while quando não sabemos em que momento a execução vai parar
+
+let i = 45412316546
+while (i > 10) {
+    console.log(i.toFixed(2))
+
+    i /= 35
+}
