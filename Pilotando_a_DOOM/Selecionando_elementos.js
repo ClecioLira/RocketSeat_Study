9f2814ad8aeb5afc46a021976e61ee9cc8e6828d
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-03-08 20:08:24 -0300

    update

---
+++ Pilotando_a_DOOM/Selecionando_elementos.js
@@ -0,0 +1,14 @@
+// getElementById() (Element)
+document.getElementById("string dentro do ID no HTML")
+
+// getElementsByClassName() (HTMLCollection)
+document.getElementsByClassName("string dentro da CLASS no HTML")
+
+// getElementsByTagName() (HTMLCollection)
+document.getElementsByTagName("nome da TAG no HTML")
+
+// querySelector() (Element)
+document.querySelector("seleciona pelo ID, pela CLASS ou pelo nome da TAG no HTML")
+
+// querySelectorAll() (Nodelist)
+document.querySelectorAll("seleciona todos os seletores de acordo com o ID, CLASS ou nome da TAG no HTML")
