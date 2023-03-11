2e7343c66412aa9cbf9c8a2064fb2c6eed42e7db
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-03-10 22:38:56 -0300

    update

--- Pilotando_a_DOOM/Navegando_pelos_Elementos.js
+++ Pilotando_a_DOOM/Navegando_pelos_Elementos.js
@@ -1,9 +1,11 @@
 // Navegando pelos elementos
+// PAIS
 // parentNode/ parentElement -> procurar o pai do seletor selecionado
 const body = document.querySelector("body")
 console.log(body.parentNode) // Procurando qual é o pai do body
 
 
+// FILHOS
 // childNodes/ children -> pegar todos os filhos do seletor selecionado
 console.log(body.childNodes)
 console.log(body.children) // HTMLcollection
@@ -13,3 +15,11 @@ console.log(body.firstElementChild)
 
 // lastChild/ lastElementChild -> Escolhe o último filho do seletor selecionado
 console.log(body.lastElementChild)
+
+
+// IRMÃOS
+// nextSibling/ nextElementSibling -> Pega o irmão próximo ao seletor selecionado
+console.log(body.nextElementSibling)
+
+// previousSibling/ previousElementSibling -> Pega o irmão anterior ao seletor selecionado
+console.log(body.previousElementSibling)
