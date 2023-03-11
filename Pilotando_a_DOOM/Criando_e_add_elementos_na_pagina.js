0413b17482d50426f13cc05d93f64dfdc92e5246
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-03-10 22:59:43 -0300

    UPDATE

--- Pilotando_a_DOOM/Criando_e_add_elementos_na_pagina.js
+++ Pilotando_a_DOOM/Criando_e_add_elementos_na_pagina.js
@@ -1,9 +1,19 @@
 // Criando e adicionando elementos na página
+// CRIAR
 // createElement
 const div = document.createElement("div") // criei uma div no HTML
 div.innerText = "Olá, mundo!"
 
+
+// ADICIONAR
 // append/ prepend
 const body = document.querySelector("body")
 body.append(div) // adicionei o elemento na última linha do body no HTML
 body.prepend(div) // adicionei o elemento na primeira linha do body no HTML
+
+// insertBefore -> adicionar o novo elemento antes do seletor selecionado
+const script = body.querySelector("script")
+//                el criado  seletor selecionado
+body.insertBefore(div,       script)
+// gambiarra para adicionar o novo elemento após o seletor selecionado
+body.insertBefore(div, script.nextElementSibling)
