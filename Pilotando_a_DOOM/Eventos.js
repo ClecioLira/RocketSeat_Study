a76570739d1fb21e6e2319967aa1f93c9d952a5e
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-03-11 16:26:53 -0300

    update

---
+++ Pilotando_a_DOOM/Eventos.js
@@ -0,0 +1,12 @@
+// Eventos - on
+
+
+// Evento para teclado - onKeydown -> apertei a tecla
+//                       onKeyup/  -> soltei a tecla
+//                       onKeypress -> pressionei a tecla
+
+
+// Adicionando eventos via JS -> addEventListener()
+const h1 = document.querySelector("h1")
+//                 parâmetro
+h1.addEventListener("click", funçao())
