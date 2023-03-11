43d51e5ff5463b214feec4198df4d6fb0d45a48e
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-03-10 22:16:25 -0300

    update

--- Pilotando_a_DOOM/Manipulando_estilos_e_classes.js
+++ Pilotando_a_DOOM/Manipulando_estilos_e_classes.js
@@ -3,3 +3,12 @@
 const element = document.querySelector("body")
 element.style.backgroundColor = blue
 element.style.color = white
+
+
+// ClassList
+// Ele olha as classes que tem no seletor selecionado, remove(revome()) classes, adiciona(add()) classes, toggle() ele remove a class se exitir ou adiciona se não existir.
+const elemento = document.querySelector("body")
+elemento.classList.add("green")
+elemento.classList.remove("green")
+elemento.classList.toggle("green")
+console.log(elemento.classList)
