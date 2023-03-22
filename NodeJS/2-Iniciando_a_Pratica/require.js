b7689ad109020dab0e8821ea27c3bdc277eb9a98
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-03-22 12:09:22 -0300

    update

--- NodeJS/2-Iniciando_a_Pratica/require.js
+++ NodeJS/2-Iniciando_a_Pratica/require.js
@@ -2,6 +2,6 @@ const path = require("path")
 console.log(path.basename(__filename))
 
 
-// Chamando um modulo de outro arquivo criado por mim mesmo
+// Chamando um modulo de outro arquivo criado por mim
 const myModule = require("./exports")
 console.log(myModule)
