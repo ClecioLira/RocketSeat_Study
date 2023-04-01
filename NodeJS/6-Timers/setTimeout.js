4b3c607166d941c000af645cf42fdd39ec2c52ce
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-03-31 23:16:58 -0300

    update

--- NodeJS/6-Timers/setTimeout.js
+++ NodeJS/6-Timers/setTimeout.js
@@ -1,8 +1,14 @@
 // setTimeout roda uma função depois de X milissegundos
 
-const timeout = 3000
-const finished = () => console.log("Demoro o tempo do timeout para aparecer")
+const timeout = 2000
+const timeout2 = 4000
+const timeout3 = 6000
+const msg1 = () => console.log("Tudo bem?")
+const msg2 = () => console.log("Como foi seu dia?")
+const msg3 = () => console.log("Aprendeu algo novo?")
 
-setTimeout(finished, timeout)
+setTimeout(msg1, timeout)
+setTimeout(msg2, timeout2)
+setTimeout(msg3, timeout3)
 // Assincronismo
-console.log("Mostrar!")
+console.log("Olá")
--- NodeJS/7-Events/EmitirEventos.js
+++ NodeJS/7-Events/EmitirEventos.js
@@ -1,4 +1,11 @@
 const {EventEmitter} = require('events');
 const emitir = new EventEmitter();
 
-emitir.emit("Diga algo!") // Aqui eu emitir o evento porém falta eu ouvir ele
+emitir.on("nome do emit", (mensagem) => {
+    console.log("Eu ouvi você!", mensagem)
+}) // Aqui é onde eu ouço o evento
+
+emitir.emit("nome do emit", "Oi amor!!") // Aqui eu emitir o evento porém falta eu ouvir ele
+emitir.emit("nome do emit", "Estou com sodadi")
+emitir.emit("nome do emit", "Muita sodadi")
+emitir.emit("nome do emit", ":( :(")
