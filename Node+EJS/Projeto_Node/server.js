506e7abfc99cf0bfc82466444c50e9f2a168c54b
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-04-08 21:42:09 -0300

    update

--- Node+EJS/Projeto_Node/server.js
+++ Node+EJS/Projeto_Node/server.js
@@ -4,7 +4,39 @@ const app = express()
 app.set("view engine", "ejs")
 
 app.get("/", function(req, res) {
-    res.render("pages/index")
+    const items = [
+        {
+            title: "D",
+            message: "Desenvolver aplicações de forma fácil"    
+        },
+        {
+            title: "E",
+            message: "EJS usa JavaScript para renderizar HTML"
+        },
+        {
+            title: "M",
+            message: "Muito fácil de usar"
+        },
+        {
+            title: "A",
+            message: "Amorzinho"
+        },
+        {
+            title: "I",
+            message: "install EJS"
+        },
+        {
+            title: "S",
+            message: "Sintaxe simples"
+        }
+    ]
+
+    const subtitle = "É uma linguagem de modelagem para criação de páginas HTML usando JavaScript."
+
+    res.render("pages/index", {
+        quality: items,
+        subtitle: subtitle,
+    })
 })
 app.get("/sobre", function(req, res) {
     res.render("pages/about")
